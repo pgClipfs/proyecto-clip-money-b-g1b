58c9424a1ef6196e5da0d431ea5cae50ea42cbca
Lara <gimena.befer@gmail.com>
2020-11-16 18:50:59 -0300

    componente login

--- billeterafront/src/app/app.module.ts
+++ billeterafront/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
