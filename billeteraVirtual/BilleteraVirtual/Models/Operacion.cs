ea48c61ed53bf88ee26208fc290f783c9a4f367b
Eze Heredia <eze.heredia023@gmail.com>
2020-11-24 15:40:55 -0300

    Clases
    Creacion de clases banco, billetera, operaciones, transaccion

---
+++ billeteraVirtual/BilleteraVirtual/Models/Operacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BilleteraVirtual.Models
+{
+    public class Operacion
+    {
+        private int id;
+        private string tipoDeOperacion;
+
+        public Operacion(int id, string tipoDeOperacion)
+        {
+            this.id = id;
+            this.tipoDeOperacion = tipoDeOperacion;
+        }
+
+        public int Id { get => id; }
+        public string TipoDeOperacion { get => tipoDeOperacion; set => tipoDeOperacion = value; }
+    }
+}
