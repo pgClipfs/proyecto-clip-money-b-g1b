36923ad2f81a49e863b482d97639f69dde5fad98
Ezequiel <eze.aballay@gmail.com>
2020-11-24 19:55:18 -0300

    procedimientos almacenados

---
+++ scriptdb.sql
@@ -0,0 +1,120 @@
+USE [master]
+GO
+
+/****** Object:  Database [billetera_virtual]    Script Date: 24/11/2020 19:53:05 ******/
+DROP DATABASE [billetera_virtual]
+GO
+
+/****** Object:  Database [billetera_virtual]    Script Date: 24/11/2020 19:53:05 ******/
+CREATE DATABASE [billetera_virtual]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'billetera_virtual', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\billetera_virtual.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'billetera_virtual_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\billetera_virtual_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [billetera_virtual].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [billetera_virtual] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [billetera_virtual] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [billetera_virtual] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [billetera_virtual] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [billetera_virtual] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET RECOVERY SIMPLE 
+GO
+
+ALTER DATABASE [billetera_virtual] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [billetera_virtual] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [billetera_virtual] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [billetera_virtual] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [billetera_virtual] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [billetera_virtual] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [billetera_virtual] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+
+ALTER DATABASE [billetera_virtual] SET QUERY_STORE = OFF
+GO
+
+ALTER DATABASE [billetera_virtual] SET  READ_WRITE 
+GO
+
+
