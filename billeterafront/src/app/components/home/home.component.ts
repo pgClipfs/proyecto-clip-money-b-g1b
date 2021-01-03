b3ba0b045bb0d0b9a8573cc411afa90edbef742b
felisabg <felisabarberag@gmail.com>
2021-01-03 19:25:26 -0300

    Se modifica el navbar mobile y la imagen del home

--- billeterafront/src/app/components/home/home.component.ts
+++ billeterafront/src/app/components/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../app.component.css']
 })
 export class HomeComponent implements OnInit {
+  Image = '/assets/images/phone.png';
 
   constructor() { }
 
