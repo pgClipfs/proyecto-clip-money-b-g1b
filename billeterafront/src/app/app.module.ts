a2d3685fa722ee4f9808f2a9d59607a964647b90
gimenab <gimena.befer@gmail.com>
2021-01-18 18:18:58 -0300

    se sube componente main

--- billeterafront/src/app/app.module.ts
+++ billeterafront/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SecretanswerComponent } from './components/secretanswer/secretanswer.component';
 import { UsuarioLoginService } from './services/usuario-login.service';
+import { MainComponent } from './components/main/main.component';
 
 
 @NgModule({
@@ -30,6 +31,7 @@ import { UsuarioLoginService } from './services/usuario-login.service';
     MyaccountComponent,
     ResetpasswordComponent,
     SecretanswerComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
